9b931ea8c5437b80bbe100a4d39d0e5237ca97e3
dolphius21 <johnrudolphlee@gmail.com>
2021-04-29 09:16:16 +0800

    done with like component

--- src/components/common/like.js
+++ src/components/common/like.js
@@ -1,24 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
-// const Like = ({ props }) => {
-//   return !props.isLiked ? (
-//     <FaHeart onClick={!props.onLike} style={{ cursor: 'pointer' }} />
-//   ) : (
-//     <FaRegHeart onClick={!props.onLike} style={{ cursor: 'pointer' }} />
-//   );
-// };
-
-// export default Like;
-
-class Like extends Component {
-  render() {
-    return this.props.isliked ? (
-      <FaHeart onClick={this.props.onLike} style={{ cursor: 'pointer' }} />
-    ) : (
-      <FaRegHeart onClick={this.props.onLike} style={{ cursor: 'pointer' }} />
-    );
-  }
-}
+const Like = (props) => {
+  return props.isLiked ? (
+    <FaHeart onClick={props.onLike} style={{ cursor: 'pointer' }} />
+  ) : (
+    <FaRegHeart onClick={props.onLike} style={{ cursor: 'pointer' }} />
+  );
+};
 
 export default Like;
