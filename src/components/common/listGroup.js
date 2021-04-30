f2fd258abcc2c10cf23815f777abbcc7926f781d
dolphius21 <johnrudolphlee@gmail.com>
2021-04-30 12:20:56 +0800

    done with listGroup component

---
+++ src/components/common/listGroup.js
@@ -0,0 +1,32 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const ListGroupComp = (props) => {
+  const {
+    items,
+    textProperty,
+    valueProperty,
+    onItemSelect,
+    selectedItem
+  } = props;
+
+  return (
+    <ListGroup>
+      {items.map((item) => (
+        <ListGroup.Item
+          key={item[valueProperty]}
+          onClick={() => onItemSelect(item)}
+          active={item === selectedItem}
+        >
+          {item[textProperty]}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
+
+ListGroupComp.defaultProps = {
+  textProperty: 'name',
+  valueProperty: '_id'
+};
+
+export default ListGroupComp;
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import Like from './common/like';
+import ListGroupComp from './common/listGroup';
 import PaginationComp from './common/pagination';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { paginate } from '../utils/paginate';
 import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
 
 class Movies extends Component {
   state = {
-    movies: getMovies(),
+    movies: [],
+    genres: [],
     currentPage: 1,
     pageSize: 4
   };
+
+  componentDidMount() {
+    const genres = [{ name: 'All Genres' }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
+  }
 
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -30,56 +40,84 @@ class Movies extends Component {
     this.setState({ currentPage: page });
   };
 
+  handleGenreSelect = (genre) => {
+    this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+
   render() {
     const { length: count } = this.state.movies;
-    const { movies: allMovies, pageSize, currentPage } = this.state;
+    const {
+      movies: allMovies,
+      pageSize,
+      currentPage,
+      selectedGenre
+    } = this.state;
+
     if (count === 0) return <p>There are no movies in the database.</p>;
-    const movies = paginate(allMovies, currentPage, pageSize);
+
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((movies) => movies.genre._id === selectedGenre._id)
+        : allMovies;
+
+    const movies = paginate(filtered, currentPage, pageSize);
+
     return (
       <main>
-        <p>Showing {count} movies in the database.</p>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th colSpan="2">Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <td colSpan="2">{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={movie.liked}
-                    onLike={() => this.handleLike(movie)}
-                  />
-                </td>
-                <td>
-                  <Button
-                    variant="danger"
-                    onClick={() => this.handleDelete(movie)}
-                  >
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-        <PaginationComp
-          itemsCount={count}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        />
+        <Row>
+          <Col className="col-3">
+            <ListGroupComp
+              items={this.state.genres}
+              selectedItem={this.state.selectedGenre}
+              onItemSelect={this.handleGenreSelect}
+            />
+          </Col>
+          <Col>
+            <p>Showing {filtered.length} movies in the database.</p>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th colSpan="2">Title</th>
+                  <th>Genre</th>
+                  <th>Stock</th>
+                  <th>Rate</th>
+                  <th />
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {movies.map((movie) => (
+                  <tr key={movie._id}>
+                    <td colSpan="2">{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                      <Like
+                        liked={movie.liked}
+                        onLike={() => this.handleLike(movie)}
+                      />
+                    </td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        onClick={() => this.handleDelete(movie)}
+                      >
+                        Delete
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+            <PaginationComp
+              itemsCount={filtered.length}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange}
+            />
+          </Col>
+        </Row>
       </main>
     );
   }
