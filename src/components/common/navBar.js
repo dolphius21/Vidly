14d432eec8aecf92042bdf8ea6f633522c999083
dolphius21 <johnrudolphlee@gmail.com>
2021-05-01 13:35:35 +0800

    fixed navbar issue

--- src/components/common/navBar.js
+++ src/components/common/navBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <NavLink className="nav-item nav-link" to="/">
+          <NavLink className="nav-item nav-link" to="/movies">
             Movies
           </NavLink>
           <NavLink className="nav-item nav-link" to="/customers">
